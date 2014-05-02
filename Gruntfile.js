41e2b8b28ab358a55bb57783c81f1f6af54674c5
Stewart Knapman <info@stewartknapman.com>
2014-05-02 14:54:05 +1200

    add bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,22 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'assets/css/style.css': 'src/scss/style.scss'
+        }
+      }
     }
   });
   
   // Load task plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['sass']);
 };
